43705f91ddf15ef6300f8cdfeba02b941704aae1
acaaa8 <150428950+acaaa8@users.noreply.github.com>
2024-03-30 12:36:29 +0700

    minute_log.sh

--- soal_4/minute_log.sh
+++ soal_4/minute_log.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-### TAMBAHKAN INI KE CRONTAB
-# * * * * * /home/aca/Dokumen/SISOP/Modul1/soal_nomor_4/minute_log.sh
+### ADD THIS TO THE CRONTAB
+# * * * * * /home/aca/Documents/SISOP/Modul1/soal_nomor_4/minute_log.sh
 
-# mengekstraksi penggunaan memori ke file teks sementara
+# extracting memory usage to a temporary text file
 free -m > temp.txt
 
-# mengekstraksi penggunaan disk dari direktori home pengguna ke file teks sementara
-pengguna=whoami && du -sh /home/$pengguna >> temp.txt
+# extracting disk usage from the user home directory to THE temporary text file
+user=whoami && du -sh /home/$user >> temp.txt
 
-# mencetak hasil menggunakan echo dan awk
-echo "mem_total,mem_used,mem_free,mem_shared,mem_buff,mem_available,swap_total,swap_used,swap_free,path,path_size" > /home/$pengguna/log/"metrics_$(date +"%Y%m%d%H%M%S").log"
+# print results using echo and awk
+echo "mem_total,mem_used,mem_free,mem_shared,mem_buff,mem_available,swap_total,swap_used,swap_free,path,path_size" > /home/$user/log/"metrics_$(date +"%Y%m%d%H%M%S").log"
 awk '/Mem/ {print $2","$3","$4","$5","$6","$7","};
      /Swa/ {print $2","$3","$4","};
      /home/ {print $2","$1}
-     ' temp.txt | paste -s -d '' >> /home/$pengguna/log/"metrics_$(date +"%Y%m%d%H%M%S").log"
+     ' temp.txt | paste -s -d '' >> /home/$user/log/"metrics_$(date +"%Y%m%d%H%M%S").log"
 
-# mengelola izin
-chmod go-rwx "/home/$pengguna/log/""metrics_$(date +"%Y%m%d%H%M%S").log"    
+# manage permissions
+chmod go-rwx "/home/$user/log/""metrics_$(date +"%Y%m%d%H%M%S").log"    
 
-# menghapus file teks sementara
+# delete the temporary text file
 rm temp.txt
-
