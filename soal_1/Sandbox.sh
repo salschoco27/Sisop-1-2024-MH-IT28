32c84309ecb2cd8d07d46f320705ed9954b59546
salsa <151063684+salschoco27@users.noreply.github.com>
2024-03-30 16:22:33 +0700

    Update Sandbox.sh

--- soal_1/Sandbox.sh
+++ soal_1/Sandbox.sh
@@ -12,6 +12,8 @@ sort -t ',' -k20,20 Sandbox.csv | head -n 3 |awk -F ',' '{print $6 "     " $20}'
 echo "   "
 
 echo "3 Kategori Produk dengan Profit Tertinggi"
+#(sebelum revisi)
+#awk -F ',' '{sum[$14]+=$20} END {for (i in sum) print i "=" sum[i]}' Sandbox.csv | sort -t ',' -k 14nr | head -4
 awk -F ',' 'NR>1 {sum[$14]+=$20} END {for (i in sum) print i "=" sum[i]}' Sandbox.csv | sort -t ',' -k 14nr | head -4
 echo "   "
 
